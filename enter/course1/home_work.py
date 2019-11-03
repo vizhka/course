a998a19cffd6f23189d99d9e545cff9a9eebc4bd
Vizhka <piter.seredenko@gmail.com>
2019-11-04 02:33:16 +0300

    Задал пару вопросов

--- enter/course1/home_work.py
+++ enter/course1/home_work.py
@@ -6,6 +6,9 @@ Age = input('How old are you, ' + name + '?')
 Age_num = int(Age)
 if Age_num >= 25:
     print('You are so old!' + name)
+    input('You will do a great things, drain cookie!' +
+          'DO you?')
+    print('Well done,' + name)
 if Age_num <= 25:
     print('You are young!' + name)
 

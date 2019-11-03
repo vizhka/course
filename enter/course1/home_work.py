dd6915d6678cb5a3c496e99a003ed2fb3824ff6c
Vizhka <piter.seredenko@gmail.com>
2019-11-04 02:10:18 +0300

    Some new

--- enter/course1/home_work.py
+++ enter/course1/home_work.py
@@ -3,8 +3,11 @@ print(name + ', Nice to meet You!')
 a = input('What is the University do you study?:')
 print(a + ' - wow! it`s great!')
 Age = input('How old are you, ' + name + '?')
-print({name} + ', you are old!')
-
+Age_num = int(Age)
+if Age_num >= 25:
+    print('You are so old!' + name)
+if Age_num <= 25:
+    print('You are young!' + name)
 
 
 

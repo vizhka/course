f77f9ceca6d14d5f496096f30797b505327f3104
Vizhka <piter.seredenko@gmail.com>
2019-11-04 01:50:07 +0300

    Some new

--- enter/course1/home_work.py
+++ enter/course1/home_work.py
@@ -3,8 +3,8 @@ print(name + ', Nice to meet You!')
 a = input('What is the University do you study?:')
 print(a + ' - wow! it`s great!')
 Age = input('How old are you, ' + name + '?')
-age_num = int(Age)
-print({name} + ', you are old!' + 'You are lived -' + {age_num*12*30*24*60} + 'minutes')
+print({name} + ', you are old!')
+
 
 
 

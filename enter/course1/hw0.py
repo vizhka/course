1ceb629a7364ba1d7fb7c75e7ef966784f1e9a5b
Vizhka <piter.seredenko@gmail.com>
2019-11-04 22:29:50 +0300

    Пофиксил задачку

--- enter/course1/hw0.py
+++ enter/course1/hw0.py
@@ -2,16 +2,17 @@ name = input('Hello! Please enter your name:')
 print(name + ', Nice to meet You!')
 a = input('What is the University do you study?:')
 print(a + ' - wow! it`s great!')
-Age = int(input('How old are you, ' + name + '?'))
+Age = int(input('Enter your age: '))
 
 if Age >= 25:
-    print('You are so old!' + name +
-          'You are lived for' + Age * 365 + ' days!')
+    print(f'You have lived for {Age * 365} days!')
     input('You will do a great things, drain cookie!' +
           'DO you?')
     print('Well done,' + name)
 if Age <= 25:
     print('You are young!' + name)
+    print(f'You are lived for {Age * 365} days!')
+
 
 
 

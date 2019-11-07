a345815b795edc7736038a051194bd0873f81857
Vizhka <piter.seredenko@gmail.com>
2019-11-07 04:07:54 +0300

    +dicts, lists, tuples edited

--- enter/course1/hw_p2_1.py
+++ enter/course1/hw_p2_1.py
@@ -1,3 +1,13 @@
+# Создать лист из 6 любых чисел. Отсортировать его по возрастанию
 my_list = [15, 32, 43, 54, 80, 46]
 my_list.sort()
 print(my_list)
+# Make a tuple from 10 random equals number, find max and min
+t = (2.57, 3.77, 2.11, 4.55, 6.11, 6.87, 4.11, 6.98, 11.54, 18.66)
+print(max(t))
+print(min(t))
+# Make a list from 3 words: ['Earth', 'Russia', 'Moscow'],
+l = ['Earth', 'Russia', 'Moscow']
+# Do list cheating with->
+a = str(l[0] + ' -> ' + l[1] + ' -> ' + l[2])
+print(a)
